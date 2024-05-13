5213cc31b852c44e88e508ed566621e02e48c382
Jesús Rodrigo <jesus.rodrigo@gmail.com>
2024-05-13 22:53:13 +0100

    Add guifont

--- init.lua
+++ init.lua
@@ -161,6 +161,9 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- vim.opt.guifont = 'NotoSansM Nerd Font'
+-- vim.opt.guifont = 'FiraCode Nerd Font Mono'
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
